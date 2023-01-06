b268c6f9cc09b34666e1b05e8c5aa86f269612d0
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-06 10:17:48 +0100

    feat : migrations and admin

--- ecom_project/app_users/models.py
+++ ecom_project/app_users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserClass(models.Model):
+
+    user_id = models.IntegerField()
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d')
+    name = models.CharField(max_length=100)
+    industry = models.CharField(max_length =100)
+    country = models.CharField(max_length=100)
