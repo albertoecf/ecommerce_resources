5c74231302fe8fab3b7be4007078e321879815f8
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-06 10:41:17 +0100

    feat: user list connected db

--- ecom_project/app_users/models.py
+++ ecom_project/app_users/models.py
@@ -8,3 +8,6 @@ class UserClass(models.Model):
     name = models.CharField(max_length=100)
     industry = models.CharField(max_length =100)
     country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- ecom_project/app_users/views.py
+++ ecom_project/app_users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import UserClass
 
 # Create your views here.
 def users_list_views(request):
-    return render(request, 'users_list.html')
+    users_all = UserClass.objects.all()
+
+    users_dic = {
+        'users_all' : users_all
+    }
+
+    return render(request, 'users_list.html', users_dic)
