c8686bc0f3687b0d76fe4a024c5ac17198304cab
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-09 07:42:22 +0100

    feat: display and link categories from database

--- ecom_project/app_category/models.py
+++ ecom_project/app_category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class CategoryClass(models.Model):
@@ -12,6 +13,9 @@ class CategoryClass(models.Model):
         verbose_name = "Category Class"
         verbose_name_plural = "Category Class"
 
+    def get_url(self):
+        # return sth like  : http://127.0.0.1:8000/store/product-launch
+        return reverse('app_store:products_by_category_path',args=[self.slug])
 
     def __str__(self):
         return self.category_name
