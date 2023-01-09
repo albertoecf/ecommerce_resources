f6253519818cf820eba00783924f0a4129fe955e
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-09 07:22:14 +0100

    feat: filtering store view by category

--- ecom_project/app_store/urls.py
+++ ecom_project/app_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'app_store'
 urlpatterns = [
-    path('', views.store_view, name='store_view_path')
+    path('', views.store_view, name='store_view_path'),
+    path('<slug:category_slug>', views.store_view, name='products_by_category_path')
 ]
--- ecom_project/app_store/views.py
+++ ecom_project/app_store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductClass
+from app_category.models import CategoryClass
 
 # Create your views here.
-def store_view(request):
-    products_available = ProductClass.objects.all().filter(is_available=True)
-    product_count  = products_available.count()
+def store_view(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None :
+        # url path has category value, i.e : store/data-science
+        categories = get_object_or_404(CategoryClass, slug=category_slug)
+        products_available = ProductClass.objects.filter(category=categories, is_available=True)
+        product_count  = products_available.count()
+    else:
+        products_available = ProductClass.objects.all().filter(is_available=True)
+        product_count  = products_available.count()
 
     info_to_render = {
         'products_available' : products_available,
