20385bf7dfde016697e892fba63dce2e7ed7b20e
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-09 07:03:17 +0100

    feat: store page display products

--- ecom_project/app_store/views.py
+++ ecom_project/app_store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import ProductClass
 
 # Create your views here.
 def store_view(request):
-    return render(request,'store/store_file.html')
+    products_available = ProductClass.objects.all().filter(is_available=True)
+    product_count  = products_available.count()
+
+    info_to_render = {
+        'products_available' : products_available,
+        'product_count' : product_count
+    }
+    return render(request,'store/store_file.html', info_to_render)
