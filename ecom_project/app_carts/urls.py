c0fb78ade3772159da35d4cab0e634eada690844
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-10 17:19:43 +0100

    fix: rename function

--- ecom_project/app_carts/urls.py
+++ ecom_project/app_carts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.cart_view, name='cart_view_path'),
     path('add_cart/<int:product_id>',views.add_cart_view, name='add_cart_view_path'),
-    path('remove_cart/<int:product_id>',views.remove_item_from_cart, name='remove_cart_view_path'),
+    path('remove_cart/<int:product_id>',views.remove_unit_item_from_cart, name='remove_cart_view_path'),
     path('remove_item_from_cart/<int:product_id>',views.remove_item_from_cart, name='remove_item_from_cart_view_path'),
 ]
