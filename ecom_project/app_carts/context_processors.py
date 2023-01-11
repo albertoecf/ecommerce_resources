87c7b7a3b0a78030cf2a5397fbf3b1bc743aa92e
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-11 07:57:17 +0100

    feat: navbar cart connected

---
+++ ecom_project/app_carts/context_processors.py
@@ -0,0 +1,18 @@
+from .models import CartClass, CartItemClass
+from .views import _cart_id
+
+def counter(request):
+    cart_items_count = 0
+
+    try :
+        cart_fetched = CartClass.objects.filter(cart_id=_cart_id(request))
+        cart_items = CartItemClass.objects.all().filter(cart = cart_fetched[ :1])
+        # We want the sum of all items (item_id * quantity)
+
+        for cart_item in cart_items:
+            cart_items_count  = cart_item.quantity
+
+    except CartClass.DoesNotExist:
+            cart_items_count = 0
+
+    return dict(cart_items_count=cart_items_count)
