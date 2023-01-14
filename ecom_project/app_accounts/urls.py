6eb1232fd03a1f8b96af5ceb10e9183bf776d135
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-14 09:38:35 +0100

    feat: include login page from template

--- ecom_project/app_accounts/urls.py
+++ ecom_project/app_accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = 'app_accounts'
 urlpatterns = [
     path('register/',views.register_view, name='register_view_path'),
     path('login/',views.login_view, name='login_view_path'),
