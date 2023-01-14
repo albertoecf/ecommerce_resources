3885739253de95a164a0c667a4d51d7b59beff9c
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-14 09:22:55 +0100

    feat: accounts setup

--- ecom_project/app_accounts/urls.py
+++ ecom_project/app_accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/',views.register_view, name='register_view_path'),
+    path('login/',views.login_view, name='login_view_path'),
+    path('logout/',views.logout_view, name='logout_view_path'),
     ]
