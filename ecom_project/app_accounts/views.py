0ac6c21528923bd0b634eb33d790fe05af8e3e5a
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-14 11:56:09 +0100

    feat: register new users

--- ecom_project/app_accounts/views.py
+++ ecom_project/app_accounts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .forms import RegistrationFormClass
 # Create your views here.
 def register_view(request):
-    return render(request,'accounts/register.html')
+    form =  RegistrationFormClass()
+
+    context_to_render = {'form':form}
+
+    return render(request,'accounts/register.html', context_to_render )
 
 def login_view(request):
     return render(request, 'accounts/login.html')
