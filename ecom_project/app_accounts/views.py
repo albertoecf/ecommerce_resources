ae75182f6d52e100d221e1f5a1f7ba03fee01ebb
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-17 08:14:58 +0100

    feat: log out

--- ecom_project/app_accounts/views.py
+++ ecom_project/app_accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import RegistrationFormClass
 from .models import AccountClass
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -19,33 +20,36 @@ def register_view(request):
             user = AccountClass.objects.create_user(
                 first_name=first_name_from_form,
                 last_name=last_name_from_form,
-                username = username_from_form,
+                username=username_from_form,
                 email=email_from_form,
                 password=password_from_form)
             user.save()
             messages.success(request, 'User created')
             return redirect('app_accounts:register_view_path')
-    context_to_render={'form': form}
+    context_to_render = {'form': form}
 
     return render(request, 'accounts/register.html', context_to_render)
 
 
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+        email_user_input = request.POST['email']
+        password_user_input = request.POST['password']
 
-        user  = auth.authenticate(email=email, password=password)
+        user = auth.authenticate(
+            email=email_user_input, password=password_user_input)
 
         if user is not None:
             auth.login(request, user)
             return redirect("home_view_path")
-        else :
+        else:
             messages.error(request, "Invalid credentials")
             return redirect("app_accounts:login_view_path")
 
     return render(request, 'accounts/login.html')
 
-
+@login_required(login_url = 'login_view_path')
 def logout_view(request):
-    return
+    auth.logout(request)
+    messages.success(request, "See you soon!")
+    return redirect('app_accounts:login_view_path')
