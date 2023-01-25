cbd944118c046fa2e9467ef461cd9cd05b1e4df2
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-25 08:04:12 +0100

    feat: admin pannel enhance

--- ecom_project/app_orders/admin.py
+++ ecom_project/app_orders/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import PaymentClass, OrderClass , OrderProductClass
+from .models import PaymentClass, OrderClass, OrderProductClass
 # Register your models here.
 
+
+class OrderAdminClass(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email',
+                    'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name',
+                     'last_name', 'phone', 'email']
+    list_per_page = 20
+
+
 admin.site.register(PaymentClass)
-admin.site.register(OrderClass)
+admin.site.register(OrderClass, OrderAdminClass)
 admin.site.register(OrderProductClass)
