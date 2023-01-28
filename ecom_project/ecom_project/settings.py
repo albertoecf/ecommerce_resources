60d33d95b0ef83104f882a248f5b995e3d351db8
aecf <81099969+albertoecf@users.noreply.github.com>
2023-01-28 17:46:47 +0100

    feat: loginout

--- ecom_project/ecom_project/settings.py
+++ ecom_project/ecom_project/settings.py
@@ -56,7 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 1800
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIME_OUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'ecom_project.urls'
 
